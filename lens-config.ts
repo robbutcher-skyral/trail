4c6d45a7176f612d6a800ff9b78f5de932d55555
Rob Butcher <robert.butcher@skyral.io>
2026-08-10 17:10:05 +0100

    Migration

--- lens-config.ts
+++ lens-config.ts
@@ -1,9 +1,22 @@
+import { addLayerOrderingService } from "@adk/amphi-maps";
 import {
   FeaturesConfig,
   MapConfig,
   ReactConfig,
   getLensEnvVar,
 } from "@adk/lens-react";
+import {
+  type ProtomapsFlavorName,
+  protomapsBasemap,
+} from "@adk/protomaps-basemap-style";
+
+const LOCAL_PROTOMAPS_FLAVORS: ProtomapsFlavorName[] = ["dark"];
+
+const TILES_URL = "pmtiles:///tiles/merseyside.pmtiles";
+
+const basemaps = LOCAL_PROTOMAPS_FLAVORS.map((flavor) =>
+  protomapsBasemap({ flavor, tilesUrl: TILES_URL }),
+);
 
 const REACT_CONFIG = {
   LOGO: getLensEnvVar("LOGO", ""),
@@ -21,7 +34,8 @@ const MAP_CONFIG = {
   MAX_LATITUDE: getLensEnvVar("MAX_LATITUDE", "90"),
   MIN_LONGITUDE: getLensEnvVar("MIN_LONGITUDE", "-180"),
   MAX_LONGITUDE: getLensEnvVar("MAX_LONGITUDE", "180"),
-  TILE_SOURCE_NAME: getLensEnvVar("TILE_SOURCE_NAME", ""),
+  TILE_SERVER_URL: getLensEnvVar("TILE_SERVER_URL", "http://localhost:3000"),
+  TILE_SOURCE_NAME: getLensEnvVar("TILE_SOURCE_NAME", "merseyside"),
   BOUNDING_BOX: [
     getLensEnvVar("MIN_LONGITUDE", "-1.6"),
     getLensEnvVar("MIN_LATITUDE", "50.5"),
@@ -31,13 +45,19 @@ const MAP_CONFIG = {
   // Set the zoom range for your map
   MIN_ZOOM: getLensEnvVar("MIN_ZOOM", "2"),
   MAX_ZOOM: getLensEnvVar("MAX_ZOOM", "20"),
-  OPERATORS: [],
+  OPERATORS: [
+    addLayerOrderingService(
+      [["background", "middle", "foreground"]],
+      "background",
+    ),
+  ],
   BASEMAPS: [
     "protomaps-dark",
     "protomaps-light",
     "protomaps-white",
     "protomaps-grayscale",
     "osm-positron",
+    ...basemaps,
   ],
 } satisfies MapConfig;
 
