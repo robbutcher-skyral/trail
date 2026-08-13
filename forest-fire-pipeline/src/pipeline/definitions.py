995a8d9675343e9089fa9f026f4d2e965c4e39fc
Rob Butcher <robert.butcher@skyral.io>
2026-08-13 17:50:47 +0100

    Record store

---
+++ forest-fire-pipeline/src/pipeline/definitions.py
@@ -0,0 +1,28 @@
+from dagster import Definitions, load_assets_from_package_module
+
+from skyral.record_store_utils.dagster import RecordStoreResource
+
+from pipeline import defs as d
+
+
+record_store = RecordStoreResource(url="record_store")
+
+defs = Definitions(
+    assets=load_assets_from_package_module(d),
+    jobs=[
+        record_store.define_asset_job(
+            name="tree_coverage",
+            description="Download tree coverage data and upload to SPORE Storage",
+            pipeline_id="job-tree-coverage",
+            pipeline_metadata={
+                "kind": "pipeline",
+                "name": "tree_coverage",
+                "description": "Download tree coverage data and upload to SPORE Storage",
+                "repository": "https://github.com/robbutcher-skyral/trail.git",
+            },
+        ),
+    ],
+    resources={
+        "record_store": record_store,
+    },
+)
--- forest-fire-pipeline/src/pipeline/defs/tree_coverage_tif.py
+++ forest-fire-pipeline/src/pipeline/defs/tree_coverage_tif.py
@@ -1,14 +1,23 @@
 import dagster as dg
 from pathlib import Path
 from upath import UPath
+from skyral.record_store_utils.dagster import RecordStoreResource, with_metadata_records
 
 class TreeCoverageConfig(dg.Config):
     src_url: str = (Path(__file__).resolve().parent.parent / "treecover2010_60N_010W.tif").as_uri()
     destination_url: str = (Path(__file__).resolve().parent.parent / "treecover2010_60N_010W_new.tif").as_uri()
 
 @dg.asset
+@with_metadata_records(
+    job_id="job-tree-coverage",
+    job_metadata={
+        "description": "Download tree coverage data and upload to SPORE Storage",
+    }
+)
 def tree_coverage_loader(
+    context: dg.AssetExecutionContext,
     config: TreeCoverageConfig,
+    record_store: RecordStoreResource
 ) -> str:
     
     tif_upath = UPath(config.src_url)
@@ -23,4 +32,15 @@ def tree_coverage_loader(
     with src.open("rb") as fsrc, dst.open("wb") as fdst:
         fdst.write(fsrc.read())
 
+    record_store.add_run_metadata(
+        {
+            "src_url": config.src_url,
+        }
+    )
+    record_store.add_dataset_metadata(
+        {
+            "totalCount": 1,
+        },
+    )
+    
     return config.destination_url
